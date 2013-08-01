6f357a0721eaa74db758738d2704394346fd038f
VoR <vor@vor.com>
2013-08-01 12:03:07 -0400

    Reading image out of an rar and resizing it before displaying the contents into an image view.

--- jni/com_sketchpunk_jniunrar_unrar.cpp
+++ jni/com_sketchpunk_jniunrar_unrar.cpp
@@ -1,7 +1,5 @@
 #include <com_sketchpunk_jniunrar_unrar.h>
 #include <cstddef> //Needed else get an error about NULL Being undefined.
-//#include <stdlib.h>
-//#include <cstdlib>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -13,22 +11,45 @@
 #include "dll.hpp" //Structs are in this file
 
 //Define shortcuts to android's c version of System.out.println
-#define  LOG_TAG "jnirar"
+#define  LOG_TAG "jniunrar"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
 /*===================================================================================================*/
-//Declare some private functions
+//Declare some private functions and classes
 int EndsWith(const char *str, const char *suffix);
 std::string newLowerStr(char* cstr);
 std::vector<std::string> jStringToVector(JNIEnv *env, jstring jstr);
 
+//Byte array is going to be used as a buffer for the bytes of a rar entry
+//Since the callback run more then once once, this object maintains state of
+//position and its data.
+class ByteArray{
+	public:
+		ByteArray(size_t len){ pCurPos = 0; mLen = len; mAry = new unsigned char[mLen];}
+		~ByteArray(){ if (mAry) delete [] mAry; }
+
+		unsigned char* getArray() const { return mAry; }
+
+		void append(unsigned char *ary, size_t len){
+			if((len + pCurPos) > mLen) return; //Make sure we do not go over the allotted space for this array.
+			memcpy(mAry+pCurPos,ary,len);
+			pCurPos += len;
+		}//func
+	private:
+		unsigned char *mAry;
+		size_t mLen;
+		size_t pCurPos; //Current position in the array to append.
+};
+
+int CALLBACK rarEventCallBack(UINT msg,LPARAM UserData,LPARAM P1,LPARAM P2);
 
 /*===================================================================================================*/
 //Get some weird warning in the console that JNI_Onload is not found, added it just incase.
+//http://developer.android.com/training/articles/perf-jni.html#native_libraries
 jint JNI_OnLoad(JavaVM* vm, void* reserved){
 		JNIEnv* env;
-		if (vm->GetEnv((void**) &env, JNI_VERSION_1_6) != JNI_OK) return -1;
+		if(vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) return -1;
 		return JNI_VERSION_1_6;
 }//func
 void JNI_OnUnload(JavaVM *, void *){}
@@ -43,12 +64,9 @@ JNIEXPORT jstring JNICALL Java_com_sketchpunk_jniunrar_unrar_getVersion(JNIEnv *
 
 
 /*===================================================================================================*/
-//TODO, use string.find to filter out entries. Save time filtering at this level then to do it after the fact in java.
-//http://www.cplusplus.com/reference/string/string/find_last_of/
 JNIEXPORT jobjectArray JNICALL Java_com_sketchpunk_jniunrar_unrar_getEntries(JNIEnv * env,jclass cls,jstring jArcPath,jstring jExtList){
 	jobjectArray rtn = NULL;
 	
-	//std::string extList;
 	std::vector<std::string> extList;
 	int extListLen = 0;
 	
@@ -59,12 +77,6 @@ JNIEXPORT jobjectArray JNICALL Java_com_sketchpunk_jniunrar_unrar_getEntries(JNI
 		for(int i=0; i < extListLen; i++){
 			LOGI(extList[i].c_str());
 		}//for
-
-		//extList = jstring2string(env,jExtList);
-		//filterEnabled = 1;
-		//LOGI(extList.c_str());
-
-		//splitToVector(extList.c_str());		
 	}//if
 
 	//-----------------------------------
@@ -110,7 +122,7 @@ JNIEXPORT jobjectArray JNICALL Java_com_sketchpunk_jniunrar_unrar_getEntries(JNI
 			if(result) LOGE("Unable to process %s, error: %d", header.FileName, result); //error skipping content
 		}//while
 
-		RARCloseArchive(rHandle); //Close file, done reading from it.
+		RARCloseArchive(rHandle); //Close file
 
 		//...............................
 		//Save vector back to a basic string array that java can understand.
@@ -130,7 +142,6 @@ JNIEXPORT jobjectArray JNICALL Java_com_sketchpunk_jniunrar_unrar_getEntries(JNI
 	//-----------------------------------
 	//Cleanup
 	env->ReleaseStringUTFChars(jArcPath,arcPath);
-
 	return rtn;
 }//func
 
@@ -176,7 +187,79 @@ of the functionality is broken out, like there's an open, process and close func
 JNIEXPORT jbyteArray JNICALL Java_com_sketchpunk_jniunrar_unrar_extractEntryToArray(JNIEnv * env, jclass cls,jstring jArcPath,jstring jEntryName){
 	jbyteArray rtn = NULL;
 
+	//-----------------------------------
+	//Convert values to something useable in C
+	const char *arcPath = env->GetStringUTFChars(jArcPath,NULL);
+	const char *entryName = env->GetStringUTFChars(jEntryName,NULL);
+
+	//Setup Rar struct
+	RAROpenArchiveData stRar;
+	memset(&stRar,0,sizeof(RAROpenArchiveData));
+	stRar.ArcName = (char*)arcPath;
+	stRar.OpenMode = RAR_OM_EXTRACT;
+
+	//-----------------------------------
+	//open file with data.
+	HANDLE rHandle = RAROpenArchive(&stRar);
+	if(rHandle && !stRar.OpenResult){
+		//Setup header struct and other variables
+		int status = 0;
+		RARHeaderData header;
+		memset(&header,0,sizeof(RARHeaderData));
+
+		//...............................
+		//loop through entry items
+		while (RARReadHeader(rHandle, &header) == 0){
+			if(strcmp(header.FileName,entryName) == 0){
+				ByteArray bAry(header.UnpSize); //Create an array as big as the uncompressed size of the entry
+				RARSetCallback(rHandle,rarEventCallBack,(LPARAM)&bAry);
+
+				status = RARProcessFile(rHandle,RAR_TEST,NULL,NULL);
+				if(status == 0){
+					//Convert C++ Byte Array to something more java friendly
+					rtn = env->NewByteArray(header.UnpSize);
+					env->SetByteArrayRegion(rtn,0,header.UnpSize,(jbyte*)bAry.getArray());
+				}else{
+					LOGI("Error Extracting %s : %d",entryName,status);
+				}//if
+
+				break;
+			}else{
+				status = RARProcessFile(rHandle, RAR_SKIP, NULL, NULL); //skip entry content
+				if(status) LOGE("Unable to process %s, error: %d", header.FileName, status); //error skipping content
+			}//if
+		}//while
+
+		RARCloseArchive(rHandle); //Close file
+	}//if
+
+	//-----------------------------------
+    //Cleanup
+	env->ReleaseStringUTFChars(jEntryName,entryName);
+	env->ReleaseStringUTFChars(jArcPath,arcPath);
+
 	return rtn;
+}//func
+
+
+/*============================================================
+Callback of rar events
+============================================================*/
+//FROM MANUAL
+//Process unpacked data. It may be used to read a file while it is being extracted or tested
+//without actual extracting file to disk. Return a non-zero value to continue process
+//or -1 to cancel the archive operation
+//MSG : Event ID
+//UserData : Address to a Variable passed to the callback(in this case an instance of the bytearray object)
+//P1 : uncompressed Byte Array Chunk (will not exceed 4MB)
+//P2 : The length of the byte array of P1.
+int CALLBACK rarEventCallBack(UINT msg,LPARAM UserData,LPARAM P1,LPARAM P2){
+	if(msg == UCM_PROCESSDATA){
+		ByteArray *ary = (ByteArray*)UserData;
+		if(ary) ary->append((unsigned char*)P1,(size_t)P2);
+		else LOGE("rarEventCallBack - Could not call array append.");
+	}//if
+	return 1;
 }//func
 
 
--- src/com/sketchpunk/jniunrar/MainActivity.java
+++ src/com/sketchpunk/jniunrar/MainActivity.java
@@ -1,8 +1,15 @@
 package com.sketchpunk.jniunrar;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +18,10 @@ public class MainActivity extends Activity {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        System.out.println(unrar.getVersion());
+        //TEST: Just simple IO between Java and C++ with JNI
+        //System.out.println(unrar.getVersion());
+        
+        /*TEST : Get list of filtered files from the rar based on the list of EXT.
         String[] ary = unrar.getEntries("/sdcard/test.rar",".JPG,.PNG,.jpeg,.gif");
         if(ary != null){
         	System.out.println(ary.length);
@@ -19,6 +29,47 @@ public class MainActivity extends Activity {
         		System.out.println(ary[i]);
         	}//for
         }//if
+		*/
+
+        /*TEST: Reading image out of rar, get its width+height then scale it down and display it in an image view.*/
+        byte[] bAry = unrar.extractEntryToArray("/sdcard/test.rar","Highschool_of_the_Dead_c29-IRF\\48.jpg");
+        if(bAry != null){
+        	System.out.println(bAry.length);
+
+        	ByteArrayInputStream iStream = new ByteArrayInputStream(bAry);
+        	Bitmap bmp = null;
+        	
+        	BitmapFactory.Options bmpOption = new BitmapFactory.Options();
+        	bmpOption.inJustDecodeBounds = true; //we only want header info, not pixel
+        	BitmapFactory.decodeStream(iStream,null,bmpOption);
+        	
+        	System.out.println(bmpOption.outHeight);
+        	System.out.println(bmpOption.outWidth);
+        	
+        	int maxSize = 900;
+        	int scale = 0;
+	        	
+        	if(Math.max(bmpOption.outHeight,bmpOption.outWidth) > maxSize){
+        		if(bmpOption.outHeight > bmpOption.outWidth) scale = Math.round((float)bmpOption.outHeight / maxSize);
+        		else scale = Math.round((float)bmpOption.outWidth / maxSize);
+        	}//if
+
+        	bmpOption.inSampleSize = scale;
+        	bmpOption.inJustDecodeBounds = false;
+        	bmpOption.inScaled = false;
+        	
+        	iStream.reset();
+        	bmp = BitmapFactory.decodeStream(iStream,null,bmpOption);
+
+        	ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+        	imageView1.setImageBitmap(bmp);
+        	
+        	try{
+        		iStream.close();
+        	}catch(Exception e){}
+        }else System.out.println("no data");
+        
+        
     }//func
 
     @Override
